8a64956cc8e68a93632b1256cd4f69fd6fd33ef7
Kasper Jacobsen <k@mackwerk.dk>
2016-01-20 20:16:33 +0100

    Damnit forgot another one...

--- utils.js
+++ utils.js
@@ -6,7 +6,7 @@ var _ = require('underscore'),
 if (process.env.REDISTOGO_URL) {
   var rtg   = require('url').parse(process.env.REDISTOGO_URL),
       client = require('redis').createClient(rtg.port, rtg.hostname);
-  redis.auth(rtg.auth.split(':')[1]);
+  client.auth(rtg.auth.split(':')[1]);
 } else {
   var client = require('redis').createClient();
 }
