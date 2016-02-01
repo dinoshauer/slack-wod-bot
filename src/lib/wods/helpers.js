b59d0596630163f50ddd76ff54819419885f6d83
Kasper Jacobsen <k@mackwerk.dk>
2016-02-01 15:13:47 +0100

    Rewrite some functions to use promises

--- src/lib/wods/helpers.js
+++ src/lib/wods/helpers.js
@@ -1,24 +1,28 @@
 import _ from 'underscore';
 import moment from 'moment';
-import axios from 'axios';
+import request from 'request-promise';
 import pdfText from 'pdf-text';
 import url from 'url';
 import { createClient } from 'redis';
 
-if (process.env.REDISTOGO_URL) {
-  const rtg   = url.parse(process.env.REDISTOGO_URL);
-  const client = createClient(rtg.port, rtg.hostname);
-  client.auth(rtg.auth.split(':')[1]);
-} else {
-  const client = createClient();
+const _getClient = () => {
+  if (process.env.REDISTOGO_URL) {
+    const rtg   = url.parse(process.env.REDISTOGO_URL);
+    let client = createClient(rtg.port, rtg.hostname);
+    client.auth(rtg.auth.split(':')[1]);
+    return client;
+  } else {
+    return createClient();
+  }
 }
+
+const client = _getClient();
 
 const _page_re = /\d+?\saf\s\d+/i;
 const _email_re = /.*@crossfitcopenhagen.dk/i;
 const _footer_re = /Uklarheder.*/i;
 const _date_re = /(\d+)\.\s(\w+)/i;
 const _header_re = /WOD kalender .*/;
-
 
 const _isContent = (item) => {
   return !_page_re.test(item) &&
@@ -44,58 +48,63 @@ const _parseWod = (wod) => {
   return result;
 };
 
-export const parseWodPdf = (input, callback) => {
-  let data = {};
-  pdfText(input, (err, chunks) => {
-    if (err) {
-      return callback(err);
-    }
+const _downloadPdf = (link) => request({
+  url: link,
+  encoding: null
+});
 
-    let key;
-    _.forEach(chunks, (item) => {
-      if (_isContent(item)) {
-        var date = moment(item).locale('da');
-        if (date.isValid() && _date_re.test(item)) {
-          key = date.toISOString();
-          data[key] = [];
-        }
-        if (data.hasOwnProperty(key)) {
-          if (!_date_re.test(item)) {
-            data[key].push(item);
+const _parseWodPdf = (input) => {
+  return new Promise( (resolve, reject) => {
+    let data = {};
+    pdfText(input, (err, chunks) => {
+      if (err) {
+        reject(err);
+      }
+      let key;
+      chunks.forEach( item => {
+        if (_isContent(item)) {
+          var date = moment(item).locale('da');
+          if (date.isValid() && _date_re.test(item)) {
+            key = date.toISOString();
+            data[key] = [];
+          }
+          if (data.hasOwnProperty(key)) {
+            if (!_date_re.test(item)) {
+              data[key].push(item);
+            }
           }
         }
-      }
+      });
+      resolve(data);
     });
-    return callback(err, data);
   });
 };
 
-export const downloadPdf = (link, callback) => {
-  axios.get(link)
-    .then((res) => {
-      if (res.statusCode === 200) {
-        return callback(null, res);
-      } else {
-        return callback(err, res);
-      }
+const _saveWodList = (wods) => {
+  return new Promise( (resolve, reject) => {
+    const multiClient = client.multi();
+    Object.keys(wods).forEach( key => {
+      wods[key].forEach( (value, index) => {
+        multiClient.zadd(`wod:${key}`, index, value);
+      });
+    });
+    multiClient.exec( (err, res) => {
+      return err ? reject(err) : resolve(_.countBy(res, _.identity));
+    });
+  });
+}
+
+export const getNewWodList = (link) => (
+  _downloadPdf(link)
+    .then( data => {
+      console.log('Got the data, parsing')
+      return _parseWodPdf(data);
     })
-    .catch(callback);
-};
-
-export const saveWodList = (wods, callback) => {
-  const multiClient = client.multi();
-  _.each(wods, (value, key) => {
-    _.each(value, (item, index) => {
-      multiClient.zadd('wod:' + key, index, item);
-    });
-  });
-  multiClient.exec((err, results) => {
-    if (err) {
-      console.log(err);
-    }
-    callback(err, _.countBy(results, _.identity));
-  });
-};
+    .then( wods => {
+      console.log('Parsed the data, saving')
+      return _saveWodList(wods);
+    })
+);
 
 export const getWod = (key, callback) => {
   client.zrange('wod:' + key, 0, -1, (err, res) => {
@@ -130,7 +139,7 @@ export const presentWod = (target, wodList) => {
 
   _.forEach(wod, (value, key) => {
     wodStr += `*${key}*:\n`;
-    value.forEach((item) => item ? wodStr += `  - ${item}\n`);
+    value.forEach((item) => item ? wodStr += `  - ${item}\n` : '');
   });
   return wodStr;
 };
